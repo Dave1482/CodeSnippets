1514fcb4dd077d4cd4bca26cac03e869563f7f0d
Dave1482 <thedave1482@gmail.com>
2019-03-02 02:48:10 -0600

    Added DAInfo.m
    Includes code to show iOS device type (with plist data for easy updates)

---
+++ DeviceAndAppInfo/DAInfo.m
@@ -0,0 +1,33 @@
+#import "DAInfo.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+@synthesize infoLabel;
+
+NSString *deviceAndAppInfo()
+{
+    struct utsname systemInfo;
+    uname(&systemInfo);
+    
+    NSString *devicesPlistPath = [[NSBundle mainBundle] pathForResource:@"devices" ofType:@"plist"];
+    NSString *appVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+    NSDictionary *iOSDevices = [NSDictionary dictionaryWithContentsOfFile:devicesPlistPath];
+    NSString* deviceModel = [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
+    NSString* iOSVersion = [[UIDevice currentDevice] systemVersion];
+    NSString *info = [NSString stringWithFormat:@"%@, iOS %@\n App v%@", [iOSDevices valueForKey:deviceModel], iOSVersion, appVersion];
+    
+    return info;
+}
+
+- (void)viewDidLoad {
+    infoLabel.text = deviceAndAppInfo();
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+@end
