e4754d91f76071fb1c66fbdbd3bf6009e1d5d4f0
Dave1482 <thedave1482@gmail.com>
2019-03-02 02:55:30 -0600

    Added DAInfo.h
    Includes code to show iOS device type (with plist data for easy updates)

---
+++ DeviceAndAppInfo/DAInfo.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+#import <sys/utsname.h>
+
+@interface ViewController : UIViewController{
+    IBOutlet UILabel *infoLabel;
+}
+
+@property (nonatomic, retain) IBOutlet UILabel *infoLabel;
+
+@end
