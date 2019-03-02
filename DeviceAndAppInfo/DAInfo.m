94063b0d525e5e8a73b77a26b79c953e5849a1fb
Dave1482 <thedave1482@gmail.com>
2019-03-02 03:41:06 -0600

    Update DAInfo.m
    Prevents a "(null)" response if device is missing from devices.plist

--- DeviceAndAppInfo/DAInfo.m
+++ DeviceAndAppInfo/DAInfo.m
@@ -15,9 +15,12 @@ NSString *deviceAndAppInfo()
     NSString *devicesPlistPath = [[NSBundle mainBundle] pathForResource:@"devices" ofType:@"plist"];
     NSString *appVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
     NSDictionary *iOSDevices = [NSDictionary dictionaryWithContentsOfFile:devicesPlistPath];
-    NSString* deviceModel = [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
+    NSString* deviceModelKey = [iOSDevices valueForKey:deviceModel];
+    if (deviceModelKey == nil){
+        deviceModelKey = deviceModel;
+    }
     NSString* iOSVersion = [[UIDevice currentDevice] systemVersion];
-    NSString *info = [NSString stringWithFormat:@"%@, iOS %@\n App v%@", [iOSDevices valueForKey:deviceModel], iOSVersion, appVersion];
+    NSString *info = [NSString stringWithFormat:@"%@, iOS %@\n App v%@", deviceModelKey, iOSVersion, appVersion];
     
     return info;
 }
